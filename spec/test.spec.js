8266a66a0de25367e5ef7f303701d655f75ef10d
Daniel San <daniel.samrocha@gmail.com>
2020-12-04 00:49:25 -0300

    Add test to ensure that the AWS S3 client receives getObject as the operation in the getSignedUrl function
    Signed-off-by: Daniel San <daniel.samrocha@gmail.com>

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -340,8 +340,17 @@ describe('S3Adapter tests', () => {
       it('should get using the baseUrl and amazon using presigned URL', () => {
         options.presignedUrl = true;
         const s3 = new S3Adapter('accessKey', 'secretKey', 'my-bucket', options);
+        const originalS3Client = s3._s3Client;
+        let getSignedUrlOperation = '';
+        s3._s3Client = {
+          getSignedUrl: (operation, params, callback) => {
+            getSignedUrlOperation = operation;
+            return originalS3Client.getSignedUrl(operation, params, callback);
+          },
+        };
 
         const fileLocation = s3.getFileLocation(testConfig, 'test.png');
+        expect(getSignedUrlOperation).toBe('getObject');
         expect(fileLocation).toMatch(/^http:\/\/example.com\/files\/foo\/bar\/test.png\?/);
         expect(fileLocation).toMatch(/X-Amz-Credential=accessKey%2F\d{8}%2Fus-east-1%2Fs3%2Faws4_request/);
         expect(fileLocation).toMatch(/X-Amz-Date=\d{8}T\d{6}Z/);
@@ -373,8 +382,17 @@ describe('S3Adapter tests', () => {
         delete options.baseUrl;
         options.presignedUrl = true;
         const s3 = new S3Adapter('accessKey', 'secretKey', 'my-bucket', options);
+        const originalS3Client = s3._s3Client;
+        let getSignedUrlOperation = '';
+        s3._s3Client = {
+          getSignedUrl: (operation, params, callback) => {
+            getSignedUrlOperation = operation;
+            return originalS3Client.getSignedUrl(operation, params, callback);
+          },
+        };
 
         const fileLocation = s3.getFileLocation(testConfig, 'test.png');
+        expect(getSignedUrlOperation).toBe('getObject');
         expect(fileLocation).toMatch(/^https:\/\/my-bucket.s3.amazonaws.com\/foo\/bar\/test.png\?/);
         expect(fileLocation).toMatch(/X-Amz-Credential=accessKey%2F\d{8}%2Fus-east-1%2Fs3%2Faws4_request/);
         expect(fileLocation).toMatch(/X-Amz-Date=\d{8}T\d{6}Z/);
